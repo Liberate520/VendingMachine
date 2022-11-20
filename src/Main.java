47ec5f807f1b5cf9c193a3b6db4ad12f0a92c8f8
aafnx <aafnx@proton.me>
2022-11-20 17:51:02 +0300

    lesson-1 init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-
-//    @Liberate520
-// мой тг
 public class Main {
     public static void main(String[] args) {
-        VendingMachine vendingMachine = new VendingMachine();
-
-        Bottle bottle = new Bottle(100, "Sprite", 2);
-        System.out.println(bottle);
+        System.out.println("Hello World!");
     }
 }
