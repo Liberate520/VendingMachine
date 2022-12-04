e7cb6c7bf70837f1fc286adb0379469b822a291d
SadMeercat <igor9823@gmail.com>
2022-12-04 17:51:28 +0300

    2nd HW

--- app/Main.java
+++ app/Main.java
@@ -2,20 +2,31 @@ package app;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Family;
+import model.Father;
 import model.Human;
+import model.Mother;
+import model.Child;
 
 public class Main {
 
     public static void main(String[] args) {
-        Human mainParent = new Human("Mian");
-        List<Human> children = new ArrayList<Human>();
-        children.add(new Human("1"));
-        children.add(new Human("2"));
+        Family myFamily = new Family();
+        myFamily.addHuman(new Father("Dad"));
+        myFamily.addHuman(new Mother("Mom"));
+        myFamily.addHuman(new Child("Child 1"));
+        myFamily.addHuman(new Child("Child 2"));
+        myFamily.addHuman(new Child("Child 3"));
 
-        mainParent.AddMoreChildren(children);
-        Human tmpParent = mainParent.GetChild(1);
-        tmpParent.AddOneChild(new Human("3"));
-
-        System.out.println(mainParent.GetAllChildren(mainParent));
+        Human tmp = myFamily.findHuman("Mom");
+        if(tmp instanceof Father){
+            System.out.println(tmp.getName() + "\r\n" + myFamily.whatDo(tmp));
+        }
+        else if(tmp instanceof Mother){
+            System.out.println(tmp.getName() + "\r\n" + myFamily.whatDo(tmp));
+        }
+        else if (tmp instanceof Child){
+            System.out.println(tmp.getName() + "\r\n" + myFamily.whatDo(tmp));
+        }
     }
 }
---
+++ model/IJob.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface IJob {
+    public String Job();
+}
