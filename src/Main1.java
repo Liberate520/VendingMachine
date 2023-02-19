1e96383a86364eb2294a263a1596fc3f3bd54727
valentsk <skredovavalenti@mail.ru>
2023-02-19 21:20:29 +0300

    Первое задание

--- src/Main1.java
+++ src/Main1.java
@@ -1,7 +1,7 @@
 
 //    @Liberate520
 // мой тг
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         VendingMachine vendingMachine = new VendingMachine();
 
